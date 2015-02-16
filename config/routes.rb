320e042ae7dd62eef938d73ec09063965e55f17b
Donald Steinert <steinert.donald@gmail.com>
2015-02-16 15:59:30 -0500

    added routes, views, controllers, css for home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
+  get 'pages/home'
+  match 'home', :to => 'pages#home', :via => :get
+
+  get 'pages/about'
+  match 'about', :to => 'pages#about', :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
